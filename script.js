a403fcebae40c5e717634c46da13feeebd8120d1
Solomon-mithra <solomonmithra99@gmail.com>
2023-10-07 11:23:55 -0700

    master branch changed to main

---
+++ script.js
@@ -0,0 +1,27 @@
+import { config } from "dotenv";
+config();
+
+import { OpenAI } from "openai";
+import readline from "readline"
+
+const openai = new OpenAI({apiKey: process.env.API_KEY});
+
+const userInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const completion = await openai.chat.completions.create({
+    model:"gpt-3.5-turbo",
+    messages:[{role:"user", content:"Hello!"}]
+})
+console.log('\x1b[32m%s\x1b[0m', completion.choices[0].message.content);
+
+userInterface.on("line", async input => {
+    const completion = await openai.chat.completions.create({
+        model:"gpt-3.5-turbo",
+        messages:[{role:"user", content:input}]
+    })
+    console.log('\x1b[32m%s\x1b[0m', completion.choices[0].message.content);
+    userInterface.prompt()    
+})
